5421de45ab43be7129a450b76349660822a60075
Rasmus32 <62754460+Rasmus32@users.noreply.github.com>
2020-06-08 17:00:53 +0200

    Add files via upload

--- files.rb
+++ files.rb
@@ -1,72 +1,62 @@
-#skapa borttagna filer lista. 
 
 
-
-
-
-
-class FileObserver 
-    
-    
-    
-    
-    def initialize(target_dir)
+class FileObserver
+    def initialize(target_dir)    #Skapar alla listor och hasg
         @file_inventory = {}
         @updated_files = []
         @new_files = []
         @deleted_files = []
         @target_dir = target_dir
     end
-    
-    
+     
     def update_files
-        target_dir = "C:/Users/Rasmus.noord/Desktop/Filer/"    
+        @target_dir.gsub!("\\", "//")    #byter ut \ mot / annars funkar ej sökvägen
         path = File.join( @target_dir, "**/*" ) 
         files = Dir.glob(path) # "hämtar" alla filer
         files.each do |file|   #går igenom varje fil 
             begin 
-                if File.file?( file )
+                if File.file?( file )    #kollar ifall filen är en fil eller en mapp 
                     filetime = File.mtime(file) 
-                    #@file_inventory[file] = File.mtime(file)
-                    
                     if @file_inventory.has_key?(file)   #Har vi redan indexerat filen?
-                        p "finns redan "     
                         if @file_inventory[file] != filetime #kollar ifall mtime är desamma och kollar med indexering
                             @file_inventory[file] = filetime 
                             @updated_files << file 
                         end
                     else
-                        @file_inventory[file] = File.mtime(file)
+                        @file_inventory[file] = File.mtime(file)     #om det är en ny fil läggs den till
                         @new_files << file 
                     end
                     
                 end
             rescue => e
-                puts e.message
+                puts e.message  #felhantering
             end
         end
         
-        @file_inventory.each do |file, mtime|
+        @file_inventory.each do |file, mtime|     #kollar raderade filer
             if !files.include?(file)
-                @deleted_files << file 
+                @deleted_files << file            #lägger till i en separat lista 
                 @file_inventory.delete(file)
             end
             
         end
-        puts "Det här är raderade filer #{@deleted_files}"
+        puts "Det här är raderade filer #{@deleted_files}"       #output
         puts "-------------------------------------------"
         puts "Det här en uppdaterade filer #{@updated_files} "
         puts "-------------------------------------------"
         puts  "Det här en nya filer #{@new_files} "
         puts "-------------------------------------------"
         puts "Det här är alla filer #{@file_inventory}"
-        @new_files = []
-        @updated_files = []
+        @new_files = []       
+        @updated_files = [] 
         @deleted_files = []
-    end
-    
-    
-    
+    end  
+
 end
 
-#kolla ifall det fungerar och skapa nya filer, ändra kolla ifall det funkar. 
+
+
+
+#Utvärdering 
+#Klassen löser sin uppgift bra. 
+
--- gui.rb
+++ gui.rb
@@ -1,55 +1,49 @@
-#Kunna visa att en specifik mapp används. Se in i mappen?, Lägga till mappar, 
-#sync knapp och som sedan gör att filerna uppdateras 
-
-
-#Idag
-#Fixa en mapp som man kan kolla i med fixerade namn 
-#Sync knapp (som inte gör något)
-
+require_relative 'files.rb'     #FileObserver 
 
 require 'fox16'
 
 include Fox
 
 
-class Controller 
-  def initialize
-    @syncing_activated = false
-  end
-  
-  def syncing          #debug?
-    if @syncing_activated == false 
-      @syncing_activated = true
-      puts "Syncing Files"
-      #Synca via file observer?
-      sleep 0.5  #Bara för att härma tiden det tar för file observer att köra 
-    end
-    puts "Done syncing"         #Får någon slags resultat från Fo som slutar denna?
-    @syncing_activated = false
-  end
-  
-  
-end 
-
 
 class SyncWindow < FXMainWindow
   def initialize(app)
-    super(app, "Sync and stuff", :width => 400, :height => 300)
-    @controller = Controller.new
-    p = FXHorizontalFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_RIGHT)
+    super(app, "SyncWindow", :width => 200, :height => 100)
+
+    left_matrix = FXMatrix.new(self, 2, MATRIX_BY_ROWS | LAYOUT_FILL )    #Skapar en matrix för att lägga in knapparna 
     
-    sync_button = FXButton.new(p, "Synca",
-      :opts => BUTTON_NORMAL|LAYOUT_CENTER_X)
-      sync_button.connect(SEL_COMMAND) do |sender, sel, data|
-        @controller.syncing
+    open_dir_btn = FXButton.new(left_matrix, "Choose Directory...", :opts => BUTTON_NORMAL|LAYOUT_RIGHT)   #knapp för att välja en mapp på datorn
+    open_dir_btn.connect(SEL_COMMAND) do |sender, sel, data|
+      
+      dialog = FXDirDialog.new(self, "Choose Directory")
+      dialog.directory = ""
+      if dialog.execute != 0
+    
+        search_directory(dialog.directory)  #anropar funktion
       end
     end
     
-    def create
-      super
-      show(PLACEMENT_SCREEN)
+    update_button = FXButton.new(left_matrix, "Update", :opts => BUTTON_NORMAL|LAYOUT_LEFT)    #Knapp för att uppdatera listorna och kolla igenom sökvägen för nya/uppdaterade eller raderade filer.
+    update_button.connect(SEL_COMMAND) do |sender, sel, data|
+      @fo.update_files
     end
-  end    
+  end
+  
+  
+  
+  
+  def search_directory(dir)    #Funktionen söker igenom alla filer i den angivna sökvägen och skriver sedan ut resultatet i cmd. 
+    p dir 
+    @fo = FileObserver.new(dir)
+    @fo.update_files 
+  end 
+  
+  
+  def create
+    super
+    show(PLACEMENT_SCREEN)
+  end
+  
   
   
   
@@ -60,4 +54,12 @@ class SyncWindow < FXMainWindow
       app.run
     end
   end
-  
+end
+
+
+
+#Utvärdering
+#Klassen löser sin uppgift någorlunda bra. Man kan lätt välja sökväg och sedan lista upp alla filer och sedan uppdatera listan lätt med en knapp. 
+#Sedan skrivs listan ut i cmd så att man kan se sina filer. 
+#För att klassen ska lyckas lösa sin uppgift på bästa sätt så skulle det varit bra ifall man kunde få resultat i gui och inte i kommandotolken.
+#Samt kunde man också få in lite debug utskrifter ifall man till exempel ska updatera listan när det inte finns någon lista så att programmet inte kraschar. 
